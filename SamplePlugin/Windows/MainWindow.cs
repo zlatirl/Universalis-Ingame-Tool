b9bd0660e66cf8889812d9e7bf81c67088726f7a
zlatirl <81765175+zlatirl@users.noreply.github.com>
2025-03-25 15:02:12 +0000

    Initial commit

---
+++ SamplePlugin/Windows/MainWindow.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using Lumina.Excel.Sheets;
+
+namespace SamplePlugin.Windows;
+
+public class MainWindow : Window, IDisposable
+{
+    private string GoatImagePath;
+    private Plugin Plugin;
+
+    // We give this window a hidden ID using ##
+    // So that the user will see "My Amazing Window" as window title,
+    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
+    public MainWindow(Plugin plugin, string goatImagePath)
+        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        GoatImagePath = goatImagePath;
+        Plugin = plugin;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        // Do not use .Text() or any other formatted function like TextWrapped(), or SetTooltip().
+        // These expect formatting parameter if any part of the text contains a "%", which we can't
+        // provide through our bindings, leading to a Crash to Desktop.
+        // Replacements can be found in the ImGuiHelpers Class
+        ImGui.TextUnformatted($"The random config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+
+        if (ImGui.Button("Show Settings"))
+        {
+            Plugin.ToggleConfigUI();
+        }
+
+        ImGui.Spacing();
+
+        // Normally a BeginChild() would have to be followed by an unconditional EndChild(),
+        // ImRaii takes care of this after the scope ends.
+        // This works for all ImGui functions that require specific handling, examples are BeginTable() or Indent().
+        using (var child = ImRaii.Child("SomeChildWithAScrollbar", Vector2.Zero, true))
+        {
+            // Check if this child is drawing
+            if (child.Success)
+            {
+                ImGui.TextUnformatted("Have a goat:");
+                var goatImage = Plugin.TextureProvider.GetFromFile(GoatImagePath).GetWrapOrDefault();
+                if (goatImage != null)
+                {
+                    using (ImRaii.PushIndent(55f))
+                    {
+                        ImGui.Image(goatImage.ImGuiHandle, new Vector2(goatImage.Width, goatImage.Height));
+                    }
+                }
+                else
+                {
+                    ImGui.TextUnformatted("Image not found.");
+                }
+
+                ImGuiHelpers.ScaledDummy(20.0f);
+
+                // Example for other services that Dalamud provides.
+                // ClientState provides a wrapper filled with information about the local player object and client.
+
+                var localPlayer = Plugin.ClientState.LocalPlayer;
+                if (localPlayer == null)
+                {
+                    ImGui.TextUnformatted("Our local player is currently not loaded.");
+                    return;
+                }
+
+                if (!localPlayer.ClassJob.IsValid)
+                {
+                    ImGui.TextUnformatted("Our current job is currently not valid.");
+                    return;
+                }
+
+                // ExtractText() should be the preferred method to read Lumina SeStrings,
+                // as ToString does not provide the actual text values, instead gives an encoded macro string.
+                ImGui.TextUnformatted($"Our current job is ({localPlayer.ClassJob.RowId}) \"{localPlayer.ClassJob.Value.Abbreviation.ExtractText()}\"");
+
+                // Example for quarrying Lumina directly, getting the name of our current area.
+                var territoryId = Plugin.ClientState.TerritoryType;
+                if (Plugin.DataManager.GetExcelSheet<TerritoryType>().TryGetRow(territoryId, out var territoryRow))
+                {
+                    ImGui.TextUnformatted($"We are currently in ({territoryId}) \"{territoryRow.PlaceName.Value.Name.ExtractText()}\"");
+                }
+                else
+                {
+                    ImGui.TextUnformatted("Invalid territory.");
+                }
+            }
+        }
+    }
+}
